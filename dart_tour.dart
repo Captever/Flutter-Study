7765ddfe658802b42e43059782a0e7366852d182
CAPTEVER-GNN-PC <captever6@gmail.com>
2026-01-02 17:09:09 +0900

    add new lines

--- dart_tour.dart
+++ dart_tour.dart
@@ -3,4 +3,7 @@ void main(){
   // final API_KEY = fetchApi();
   // but this is impossible
   const API_KEY = fetchApi();
+
+  // it can use define variable especially
+  const max_allowed_participate = 120;
 }
