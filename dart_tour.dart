47cafdb37cbd7527868b1475a25324beec815891
CAPTEVER-GNN-PC <captever6@gmail.com>
2026-01-02 16:19:25 +0900

    1-1 The Var Keyword

--- dart_tour.dart
+++ dart_tour.dart
@@ -1,3 +1,8 @@
 void main() {
-  print('hello world');
+  // commonly use
+  var name = '니콜라스';
+  // after defined
+  // name = 1; // this is wrong way
+  // it can update to other value
+  name = 'nico';
 }
