e36d356e0987006d484361e64d372d55743d488c
CAPTEVER-GNN-PC <captever6@gmail.com>
2026-01-02 16:50:03 +0900

    1-4 Final Variables

--- dart_tour.dart
+++ dart_tour.dart
@@ -1,21 +1,5 @@
-// Without null safety: it cause error
-/*
-bool isEmpty(String string) => string.length == 0;
-
-main() {
-  isEmpty(null);
-}
-*/
-
-// but if with '?', it can
 void main() {
-  String? nico = 'nico';
-  nico = null;
-
-  if(nico != null){
-    print(nico.length);
-  }
-
-  // it can use more comportable
-  print(nico?.length);
+  final name = 'nico';
+  // if using final, it cause error
+  name = 'las';
 }
