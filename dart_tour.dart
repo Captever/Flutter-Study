190faa6a926c4812606d123f39b7dc75b3faf02f
CAPTEVER-GNN-PC <captever6@gmail.com>
2026-01-02 18:11:34 +0900

    1-7 Recap

--- dart_tour.dart
+++ dart_tour.dart
@@ -1,9 +1,50 @@
-void main(){
-  // this is possible
-  // final API_KEY = fetchApi();
-  // but this is impossible
-  const API_KEY = fetchApi();
+void main() {
+  // var
+  int i = 12;
+  var name = 'la';
+  i = 12121212;
+  name = 'lalall';
 
-  // it can use define variable especially
-  const max_allowed_participate = 120;
+
+  // final
+  final name2 = 'nico';
+  name2 = '121212';
+
+
+  // dynamic
+  dynamic name3;
+  name3 = '122';
+  name3 = 1223213;
+  name3 = true;
+
+  if (name3 is String){
+    print(name3.length);
+  }
+
+
+  // const
+  const api_key = '121213123123';
+  api_key = '1213123';
+
+  final String username;
+  username = getUserInput();
+
+  
+  // nullable
+  String? name4 = 'abcd';
+  name4 = null;
+  name4.isEmpty;
+
+  if (name4 != null){
+    name4.isEmpty;
+  }
+  // or
+  name4?.isEmpty;
+
+
+  // late
+  late final String name5;
+  print(name5);
+  name5 = '12';
+  print(name5);
 }
